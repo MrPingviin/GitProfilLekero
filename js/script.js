81a46cc6128432578e0960b9fbad5475139ef258
MrPingviin <foldimogyoro2000@gmail.com>
2022-06-19 16:15:22 +0200

    Átdolgozott oldal, kommentek hozzáfűzve. Végső algoritmus még hiányzik.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,35 @@
-let input = document.getElementById("input");
-let getDataBtn = document.getElementById("getDataBtn");
+let input = document.querySelector("#input");
 let url = "https://api.github.com/users/" + input.value;
+let tbody = document.querySelector("#tbody");
+let getDataBtn = document.querySelector('#getDataBtn');
 
-
-    getDataBtn.addEventListener("click", adatlekeres);
-
+// Definiálja a lekérés tulajdonságait
 let lekeres = {
     method: "GET",
     mode: "cors",
     cache: "no-cache"
 }
 
+
+/* Lekéri az adatokat fetch segítségével a megadott 'url' címről a 'lekeres' változóban tárolt tulajdonságok alapján. 
+A kapott adatokat JSON formátumba konvertálja majd azokat kiírja a konzolra. */
 function adatlekeres() {
-    fetch (url, lekeres).then(response => response.json())
-    .then(
-        data => console.log(data)
+    fetch(url, lekeres).then(response => response.json())
+        .then(
+            data => console.log(data)
         );
 }
+
+
+//Ezzel a függvénnyel bármilyen elemet létre lehet hozni
+function createAnyElement(name, attributes) {
+    let element = document.createElement(name);
+    for (let k in attributes) {
+        element.setAttribute(k, attributes[k]);
+    }
+    return element;
+}
+
+
+//Figyeli a 'getDataBtn' id-jű gombot, majd a kattintást érzékelve lefuttatja az 'adatlekeres' függvényt
+getDataBtn.addEventListener("click", adatlekeres);
